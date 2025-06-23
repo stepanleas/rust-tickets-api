094bf7e2e7023061df2b8814efcab1f0f1857393
stepanleas <s.leas@developmentaid.org>
2025-06-23 23:13:14 +0300

    rename api_routes file to api_ticket_routes

--- src/presentation/src/api/api_doc.rs
+++ src/presentation/src/api/api_doc.rs
@@ -1,8 +1,8 @@
-use crate::api::api_routes::__path_list_all;
-use crate::api::api_routes::__path_find_one;
-use crate::api::api_routes::__path_create;
-use crate::api::api_routes::__path_update;
-use crate::api::api_routes::__path_delete;
+use crate::api::api_ticket_routes::__path_list_all;
+use crate::api::api_ticket_routes::__path_find_one;
+use crate::api::api_ticket_routes::__path_create;
+use crate::api::api_ticket_routes::__path_update;
+use crate::api::api_ticket_routes::__path_delete;
 
 use utoipa::OpenApi;
 
--- src/presentation/src/api/mod.rs
+++ src/presentation/src/api/mod.rs
@@ -1,13 +1,13 @@
-mod api_routes;
+mod api_ticket_routes;
 mod api_doc;
 mod api_health_check;
 
 pub use api_doc::ApiDoc;
-pub use api_routes::find_one;
-pub use api_routes::list_all;
-pub use api_routes::create;
-pub use api_routes::update;
-pub use api_routes::delete;
+pub use api_ticket_routes::find_one;
+pub use api_ticket_routes::list_all;
+pub use api_ticket_routes::create;
+pub use api_ticket_routes::update;
+pub use api_ticket_routes::delete;
 
 pub use api_health_check::ready;
 pub use api_health_check::startup;
